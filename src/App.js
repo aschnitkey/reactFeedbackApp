ef93d44acc06c6fb780ce5dcc6135bfeaf440e57
Andrew <aschnitkey@gmail.com>
2021-12-08 13:53:54 -0500

    create first component and props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from "react";
+import PropTypes from 'prop-types';
+
+import Header from "./components/Header";
 
 function App () {
     return (
-        <h1>My App</h1>
+        <>
+        <Header text={"true"} />
+            <div className="container">
+                <h1>My App</h1>
+            </div>
+        </>
     );
+}
+
+Header.defaultProps = {
+    text: 'Feedback UI'
+}
+
+Header.propTypes = {
+    text: PropTypes.string
 }
 
 export default App;

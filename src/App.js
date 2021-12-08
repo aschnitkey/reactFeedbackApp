db41e896a9c8c02138b57d54d574e99ebe6af866
Andrew <aschnitkey@gmail.com>
2021-12-08 12:07:53 -0500

    conditionals in jsx

--- src/App.js
+++ src/App.js
@@ -1,29 +1,8 @@
 import React from "react";
 
 function App () {
-
-    const title = 'Blog Post';
-    const body = "This is my blog post.";
-    const comments = [
-        {id: 1, text: 'Comment one'},
-        {id: 2, text: 'Comment two'},
-        {id: 3, text: 'Comment three'},
-    ];
-
     return (
-        <div className="container">
-            <h1>{title.toUpperCase()}</h1>
-            <p>{body}</p>
-            
-            <div className="comments">
-                <h3>Comments ({comments.length})</h3>
-                <ul>
-                    {comments.map((comment, index) => (
-                        <li key={comment.id}>{comment.text}</li>
-                    ))}
-                </ul>
-            </div>
-        </div>
+        <h1>My App</h1>
     );
 }
 

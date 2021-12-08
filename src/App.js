afcc346066dbc78869a1a17629b4981b4e713e53
Andrew <aschnitkey@gmail.com>
2021-12-08 14:28:26 -0500

    add styles to a component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import React from "react";
-import PropTypes from 'prop-types';
 
 import Header from "./components/Header";
 
 function App () {
     return (
         <>
-        <Header text={"true"} />
+        <Header />
             <div className="container">
                 <h1>My App</h1>
             </div>
         </>
     );
-}
-
-Header.defaultProps = {
-    text: 'Feedback UI'
-}
-
-Header.propTypes = {
-    text: PropTypes.string
 }
 
 export default App;

1a04f0e2442afafbd2bd8c6baa3ae601862bb88c
Andrew <aschnitkey@gmail.com>
2021-12-10 14:20:41 -0500

    fade animation with framer motion

--- src/components/FeedbackList.js
+++ src/components/FeedbackList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { motion, AnimatePresence } from 'framer-motion';
 
 import FeedbackItem from './FeedbackItem';
 
@@ -8,22 +9,56 @@ function FeedbackList({ data, handleDelete }) {
     return <p>No Feedback Yet!</p>;
   }
 
+  ///////////////////////////////////
+  // new version with animation
+  ///////////////////////////////////
+
   return (
     <div className='feedback-list'>
-      {data.map((item) => {
-        return (
-          <FeedbackItem
-            key={item.id}
-            id={item.id}
-            rating={item.rating}
-            text={item.text}
-            handleDelete={handleDelete}
-          />
-        );
-      })}
+      <AnimatePresence>
+        {data.map((item) => {
+          return (
+            <motion.div
+              key={item.id}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+            >
+              <FeedbackItem
+                key={item.id}
+                id={item.id}
+                rating={item.rating}
+                text={item.text}
+                handleDelete={handleDelete}
+              />
+            </motion.div>
+          );
+        })}
+      </AnimatePresence>
     </div>
   );
 }
+
+///////////////////////////////////
+// original without animation
+///////////////////////////////////
+
+//   return (
+//     <div className='feedback-list'>
+//       {data.map((item) => {
+//         return (
+//           <FeedbackItem
+//             key={item.id}
+//             id={item.id}
+//             rating={item.rating}
+//             text={item.text}
+//             handleDelete={handleDelete}
+//           />
+//         );
+//       })}
+//     </div>
+//   );
+// }
 
 FeedbackList.propTypes = {
   data: PropTypes.arrayOf(

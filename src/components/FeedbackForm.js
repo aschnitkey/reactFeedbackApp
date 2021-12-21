7bdb6beabb8ec1355d5788f1561655a91866d536
Andrew <aschnitkey@gmail.com>
2021-12-21 14:22:57 -0500

    side effects with useEffect

--- src/components/FeedbackForm.js
+++ src/components/FeedbackForm.js
@@ -1,4 +1,4 @@
-import { useContext, useState } from 'react';
+import { useContext, useState, useEffect } from 'react';
 
 import Card from './shared/Card';
 import Button from './shared/Button';
@@ -11,7 +11,15 @@ function FeedbackForm() {
   const [message, setMessage] = useState('');
   const [rating, setRating] = useState(10);
 
-  const { addFeedback } = useContext(FeedbackContext);
+  const { addFeedback, feedbackEdit } = useContext(FeedbackContext);
+
+  useEffect(() => {
+    if (feedbackEdit.edit) {
+      setText(feedbackEdit.item.text);
+      setRating(feedbackEdit.item.rating);
+      setBtnDisabled(false);
+    }
+  }, [feedbackEdit]);
 
   const handleTextChange = (e) => {
     if (text === '') {
